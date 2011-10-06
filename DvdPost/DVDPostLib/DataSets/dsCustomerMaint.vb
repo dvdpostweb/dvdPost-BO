062c9188c1419653deff1f4db6d22f83a9886111
vanbelle <it@dvdpost.be>
2011-10-06 10:26:32 +0200

    change dataset

--- DvdPost/DVDPostLib/DataSets/dsCustomerMaint.vb
+++ DvdPost/DVDPostLib/DataSets/dsCustomerMaint.vb
@@ -2,4 +2,12 @@
 
 
 
+    Partial Class customer_attributesDataTable
+
+        Private Sub customer_attributesDataTable_customer_attributesRowChanging(ByVal sender As System.Object, ByVal e As customer_attributesRowChangeEvent) Handles Me.customer_attributesRowChanging
+
+        End Sub
+
+    End Class
+
 End Class
